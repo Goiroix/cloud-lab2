974c77cd0783c7002f2df5cb2e099e31535130b6
Goiroix <wipada.6685@gmail.com>
2026-03-06 14:16:42 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+echo "<h1>Lab 2: Docker Container</h1>";
+echo "Name: Wipada Deelert <br>";
+echo "Student ID: 2651032341113 <br>";
+echo "Major: Information Technology <br>";
+echo "Project: Cloud Computing Lab 2 <br>";
+?>
